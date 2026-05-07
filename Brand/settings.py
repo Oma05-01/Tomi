942fea63d68313fd4cec26035ec79c44e9ab6398
Homeboy <omaesigbone10@gmail.com>
2026-05-07 23:59:15 +0100

    static files

--- Brand/settings.py
+++ Brand/settings.py
@@ -109,9 +109,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'main', 'static'),
+    BASE_DIR / 'static',
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
